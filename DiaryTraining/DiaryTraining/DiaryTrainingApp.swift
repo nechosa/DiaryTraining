3f49575d46b78bd6b6c0ed55c590446921807e8a
i.nechesa <i.nechesa@dssl.ru>
2021-07-25 22:34:01 +0300

    Create project

---
+++ DiaryTraining/DiaryTraining/DiaryTrainingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiaryTrainingApp.swift
+//  DiaryTraining
+//
+//  Created by Ilya Nechoysa on 25.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct DiaryTrainingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
